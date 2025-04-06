9941247739024608941eac4e1e7b417921d68104
deririzz <183678853+deririzkyip@users.noreply.github.com>
2025-04-07 01:12:51 +0700

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,48 @@
+let config = {
+  "Title": "Deri Rizky",
+  "Name": "Deri Rizky",
+  "Description": "never stop trying new things.",
+  "links": [
+ 
+    {
+      "Title": "Instagram",
+      "URL": "https://www.instagram.com/deririzz",
+      "icon_classes": "fab fa-instagram"
+    },
+    {
+      "Title": "desain logo",
+      "URL": "https://www.instagram.com/permanadesign_drip",
+      "icon_classes": "fab fa-instagram"
+    },
+    {
+      "Title": "foryou",
+      "URL": "./flower.html",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Github",
+      "URL": "https://github.com/deririzkyip",
+      "icon_classes": "fab fa-github",
+    },
+    {
+      "Title": "Portofolio",
+      "URL": "https://www.behance.net/gallery/209657367/portofolio",
+      "icon_classes": "fab fa-behance"
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/in/deririzkyip",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/channel/UClqOnbWRmreghXbobrkCAnQ",
+      "icon_classes": "fab fa-youtube"
+    },
+    {
+      "Title": "Discord",
+      "URL": "https://discord.gg/kpx8gEVJ8G",
+      "icon_classes": "fab fa-discord"  
+    }
+  ]
+}
